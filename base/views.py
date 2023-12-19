5d2b185d2f9b2e31eddc2d6680a4ca16526e2a9a
macourage18 <macourage17@gmail.com>
2023-12-19 18:05:50 +0100

    latest

--- base/views.py
+++ base/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from rest_framework.decorators import  api_view
+from rest_framework.decorators import  api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from .serializers import AdvocateSerializer, CompanySerializer
 from django.db.models import Q
@@ -16,8 +17,9 @@ def endpoints(request):
 
 
 @api_view(['GET', "POST"])
+@permission_classes([IsAuthenticated])
 def advocate_list(request):
-  if request.method == 'GET':
+  if request.method == 'GET': 
     
     
     query= request.GET.get('query')
