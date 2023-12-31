6254736c2ac1135a77846c222e384bc9a9cead6c
macourage18 <macourage17@gmail.com>
2023-12-31 01:02:48 +0100

    new

--- cados_api/settings.py
+++ cados_api/settings.py
@@ -158,9 +158,8 @@ application = WhiteNoise(application)
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
+    'default': dj_database_url.parse(
+        default=os.environ.get('DATABASE_URL')
     )
 }
 
